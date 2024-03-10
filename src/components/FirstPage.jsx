f38fa8dbbc44d47a0c8613b84f5bf6b4e9296473
Pascal <pascalokereke18@gmail.com>
2024-03-10 20:23:43 +0100

    mobile view

--- src/components/FirstPage.jsx
+++ src/components/FirstPage.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 const FirstPage = () => {
     return (
         <div className='bg-black text-white min-h-screen min-w-screen flex items-center justify-center'>
-            <div className='mx-auto flex justify-center items-center relative'>
+            <div className='mx-auto max-w-lg p-8 text-center'>
             <img src="" className='w-20 h-auto  relative bottom-20 cursor-not-allowed'/>
-            <h1 className='text-4xl font-bold relative bottom-48'>Hey, It's Pascal!</h1> 
-            <p className='text-md font-medium  text-gray-300 relative right-64'>I'm a Software Engineer and Technical Writer
+            <h1 className='text-4xl font-bold mb-6'>Hey, It's Pascal!</h1> 
+            <p className='text-md font-medium  text-gray-300 leading-relaxed'>I'm a Software Engineer and Technical Writer
             <br />
             As a Software Engineer, I thrive on the challenge of building innovative solutions that solve real-world problems.
             <br />
