6ec065b1172328ce8685691a2efd079086035d6a
Pascal <pascalokereke18@gmail.com>
2024-03-10 20:47:20 +0100

    px-6

--- src/components/FirstPage.jsx
+++ src/components/FirstPage.jsx
@@ -6,7 +6,7 @@ const FirstPage = () => {
             <div className='mx-auto max-w-lg'>
             <img src="" className='w-20 h-auto relative bottom-20 cursor-not-allowed'/>
             <h1 className='text-4xl font-bold mb-6 text-center'>Hey, It's Pascal!</h1> 
-            <p className='text-md font-medium text-gray-400 px-2'>I'm a Software Engineer and Technical Writer</p>
+            <p className='text-md font-medium text-gray-400 px-6'>I'm a Software Engineer and Technical Writer</p>
             <br />
             <p className='text-md font-medium text-gray-400 px-2'>As a Software Engineer, I thrive on the challenge of building innovative solutions that solve real-world problems.</p>
             <br />
