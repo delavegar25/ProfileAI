cb5230041db4e365219524b82d0d290d9f603212
Pascal <pascalokereke18@gmail.com>
2024-03-10 20:17:59 +0100

    emoji deleted

--- src/components/FirstPage.jsx
+++ src/components/FirstPage.jsx
@@ -3,8 +3,9 @@ import React from 'react';
 const FirstPage = () => {
     return (
         <div className='bg-black text-white min-h-screen min-w-screen flex items-center justify-center'>
-            <img src="../assets/avatar.jpg" className='w-10 h-auto  relative bottom-20 cursor-not-allowed'/>
-            <h1 className='text-4xl font-bold relative bottom-12'>Hey, It's Pascal!</h1> 
+            <div className='mx-auto flex justify-center items-center relative'>
+            <img src="" className='w-20 h-auto  relative bottom-20 cursor-not-allowed'/>
+            <h1 className='text-4xl font-bold relative bottom-48'>Hey, It's Pascal!</h1> 
             <p className='text-md font-medium  text-gray-300 relative right-64'>I'm a Software Engineer and Technical Writer
             <br />
             As a Software Engineer, I thrive on the challenge of building innovative solutions that solve real-world problems.
@@ -23,7 +24,7 @@ const FirstPage = () => {
          </div>
 
         </div>
-        
+ </div>        
     );
 };
 
