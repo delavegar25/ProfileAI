0ced95837a6b6f065b1722e5f9f66c9ce75a351e
Pascal <pascalokereke18@gmail.com>
2024-03-10 21:13:20 +0100

    py-6 to heading

--- src/components/FirstPage.jsx
+++ src/components/FirstPage.jsx
@@ -5,7 +5,7 @@ const FirstPage = () => {
         <div className='bg-black text-white min-h-screen min-w-screen flex items-center justify-center'>
             <div className='mx-auto max-w-lg'>
             <img src="" className='w-20 h-auto relative bottom-20 cursor-not-allowed'/>
-            <h1 className='text-4xl font-bold mb-6 text-center'>Hey, It's Pascal!</h1> 
+            <h1 className='text-4xl font-bold mb-6 py-6 text-center'>Hey, It's Pascal!</h1> 
             <p className='text-md font-medium text-gray-400 px-6'>I'm a Software Engineer and Technical Writer</p>
             <br />
             <p className='text-md font-medium text-gray-400 px-6'>As a Software Engineer, I thrive on the challenge of building innovative solutions that solve real-world problems.</p>
