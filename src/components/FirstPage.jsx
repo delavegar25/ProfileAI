f34741fb18e54e02efd2383fe8e8fb23714cd887
Pascal <pascalokereke18@gmail.com>
2024-03-10 19:43:38 +0100

    Adding the wav emoji

--- src/components/FirstPage.jsx
+++ src/components/FirstPage.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const FirstPage = () => {
     return (
-        <div className='bg-black text-white h-screen flex items-center justify-center'>
+        <div className='bg-black text-white min-h-screen min-w-screen flex items-center justify-center'>
             <img src="../assets/avatar.jpg" className='w-10 h-auto mb-80 relative bottom-20 cursor-not-allowed'/>
             <h1 className='text-4xl font-bold mb-64 relative bottom-12'>Hey, It's Pascal!</h1> 
             <p className='text-md font-medium mb-36 text-gray-300 relative right-64'>I'm a Software Engineer and Technical Writer
@@ -13,10 +13,10 @@ const FirstPage = () => {
             <br />
             I believe that clear communication is the key to unlocking understanding, hence, I use Medium to communicate better to my audience, 
             <br />
-            breaking down web development into digestible chunks that empower readers to master new skills and concepts.                   
-            </p>
+            breaking down web development into digestible chunks that empower readers to master new skills and concepts. 
             <br />
-            Technologies: Python, Javascript(React, Vue), TailwindCSS.
+            Technologies: Python, Javascript(React & Vue), TailwindCSS.                  
+            </p>
 
          <div>
 
