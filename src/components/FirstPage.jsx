3b360dc662afec1725c1ed0432a90cd27a353425
Pascal <pascalokereke18@gmail.com>
2024-03-10 20:44:06 +0100

    gray

--- src/components/FirstPage.jsx
+++ src/components/FirstPage.jsx
@@ -6,20 +6,19 @@ const FirstPage = () => {
             <div className='mx-auto max-w-lg'>
             <img src="" className='w-20 h-auto relative bottom-20 cursor-not-allowed'/>
             <h1 className='text-4xl font-bold mb-6 text-center'>Hey, It's Pascal!</h1> 
-            <p className='text-md font-medium text-gray-600'>I'm a Software Engineer and Technical Writer</p>
+            <p className='text-md font-medium text-gray-400'>I'm a Software Engineer and Technical Writer</p>
             <br />
-            <p className='text-md font-medium text-gray-600'>As a Software Engineer, I thrive on the challenge of building innovative solutions that solve real-world problems.</p>
+            <p className='text-md font-medium text-gray-400'>As a Software Engineer, I thrive on the challenge of building innovative solutions that solve real-world problems.</p>
             <br />
-           <p >From crafting elegant code to designing user-friendly interfaces, I'm constantly pushing the boundaries of what is possible in the digital realm.</p>
+           <p className='text-md font-medium text-gray-400'>From crafting elegant code to designing user-friendly interfaces, I'm constantly pushing the boundaries of what is possible in the digital realm.</p>
             <br />
-           <p>I believe that clear communication is the key to unlocking understanding, hence, I use Medium to communicate better to my audience. </p> 
+           <p className='text-md font-medium text-gray-400'>I believe that clear communication is the key to unlocking understanding, hence, I use Medium to communicate better to my audience. </p> 
             <br />
-            <p>I break down web development into digestible chunks that empower readers to master new skills and concepts.</p> 
+            <p className='text-md font-medium text-gray-400'>I break down web development into digestible chunks that empower readers to master new skills and concepts.</p> 
             <br />
-            <p>Technologies: Python, Javascript(React & Vue), TailwindCSS.</p>                  
+            <p className='text-md font-medium text-gray-400'>Technologies: Python, Javascript(React & Vue), TailwindCSS.</p>                  
             
-
-         <div>
+         <div className=''>
 
          </div>
 
