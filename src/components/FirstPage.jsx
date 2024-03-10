8951bf1516424c228a7a6a8db21ccdcb1a123918
Pascal <pascalokereke18@gmail.com>
2024-03-10 20:41:00 +0100

    text-center heading-1

--- src/components/FirstPage.jsx
+++ src/components/FirstPage.jsx
@@ -3,14 +3,14 @@ import React from 'react';
 const FirstPage = () => {
     return (
         <div className='bg-black text-white min-h-screen min-w-screen flex items-center justify-center'>
-            <div className='mx-auto max-w-lg p-8 text-center'>
-            <img src="" className='w-20 h-auto  relative bottom-20 cursor-not-allowed'/>
-            <h1 className='text-4xl font-bold mb-6'>Hey, It's Pascal!</h1> 
-            <p className='text-md font-medium  text-gray-300'>I'm a Software Engineer and Technical Writer</p>
+            <div className='mx-auto max-w-lg'>
+            <img src="" className='w-20 h-auto relative bottom-20 cursor-not-allowed'/>
+            <h1 className='text-4xl font-bold mb-6 text-center'>Hey, It's Pascal!</h1> 
+            <p className='text-md font-medium text-gray-600'>I'm a Software Engineer and Technical Writer</p>
             <br />
-            <p className='text-md font-medium text-gray-300'>As a Software Engineer, I thrive on the challenge of building innovative solutions that solve real-world problems.</p>
+            <p className='text-md font-medium text-gray-600'>As a Software Engineer, I thrive on the challenge of building innovative solutions that solve real-world problems.</p>
             <br />
-           <p>From crafting elegant code to designing user-friendly interfaces, I'm constantly pushing the boundaries of what is possible in the digital realm.</p>
+           <p >From crafting elegant code to designing user-friendly interfaces, I'm constantly pushing the boundaries of what is possible in the digital realm.</p>
             <br />
            <p>I believe that clear communication is the key to unlocking understanding, hence, I use Medium to communicate better to my audience. </p> 
             <br />
