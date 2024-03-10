5856ddaee69b6630840051d1c2696cea3fffab45
Pascal <pascalokereke18@gmail.com>
2024-03-10 20:35:54 +0100

    git added to it

--- src/components/FirstPage.jsx
+++ src/components/FirstPage.jsx
@@ -6,18 +6,18 @@ const FirstPage = () => {
             <div className='mx-auto max-w-lg p-8 text-center'>
             <img src="" className='w-20 h-auto  relative bottom-20 cursor-not-allowed'/>
             <h1 className='text-4xl font-bold mb-6'>Hey, It's Pascal!</h1> 
-            <p className='text-md font-medium  text-gray-300'>I'm a Software Engineer and Technical Writer
+            <p className='text-md font-medium  text-gray-300'>I'm a Software Engineer and Technical Writer</p>
             <br />
-            As a Software Engineer, I thrive on the challenge of building innovative solutions that solve real-world problems.
+            <p className='text-md font-medium text-gray-300'>As a Software Engineer, I thrive on the challenge of building innovative solutions that solve real-world problems.</p>
             <br />
-            From crafting elegant code to designing user-friendly interfaces, I'm constantly pushing the boundaries of what is possible in the digital realm.
+           <p>From crafting elegant code to designing user-friendly interfaces, I'm constantly pushing the boundaries of what is possible in the digital realm.</p>
             <br />
-            I believe that clear communication is the key to unlocking understanding, hence, I use Medium to communicate better to my audience, 
+           <p>I believe that clear communication is the key to unlocking understanding, hence, I use Medium to communicate better to my audience. </p> 
             <br />
-            breaking down web development into digestible chunks that empower readers to master new skills and concepts. 
+            <p>I break down web development into digestible chunks that empower readers to master new skills and concepts.</p> 
             <br />
-            Technologies: Python, Javascript(React & Vue), TailwindCSS.                  
-            </p>
+            <p>Technologies: Python, Javascript(React & Vue), TailwindCSS.</p>                  
+            
 
          <div>
 
